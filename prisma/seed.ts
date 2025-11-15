7211f3f981155572bce5f0f631d54c3dca75a218
Daffa Ilhami <mdaffailhami@gmail.com>
2025-11-15 21:18:32 +0800

    Add database seed

---
+++ prisma/seed.ts
@@ -0,0 +1,111 @@
+import { Prisma, PrismaClient } from "@/generated/prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  // USERS
+  const users: Prisma.UserCreateInput[] = [
+    {
+      id: "u1",
+      name: "Alice",
+      email: "alice@example.com",
+      hashedPassword: "hashed123",
+    },
+    {
+      id: "u2",
+      name: "Bob",
+      email: "bob@example.com",
+      hashedPassword: "hashed456",
+    },
+  ];
+
+  // CATEGORIES
+  const categories: Prisma.ProductCategoryCreateInput[] = [
+    {
+      id: "c1",
+      name: "Clothes",
+    },
+    {
+      id: "c2",
+      name: "Electronics",
+    },
+  ];
+
+  // PRODUCTS
+  const products: Prisma.ProductCreateInput[] = [
+    {
+      id: "p1",
+      name: "T-Shirt",
+      description: "Nice cotton shirt",
+      imageUrls: ["https://dummy.com/shirt.jpg"],
+      isAvailable: true,
+      price: 100000,
+      category: { connect: { id: "c1" } },
+    },
+    {
+      id: "p2",
+      name: "Laptop",
+      description: "Fast laptop",
+      imageUrls: ["https://dummy.com/laptop.jpg"],
+      isAvailable: true,
+      price: 15000000,
+      category: { connect: { id: "c2" } },
+    },
+  ];
+
+  // ORDERS
+  const orders: Prisma.OrderCreateInput[] = [
+    {
+      id: "o1",
+      isPaid: false,
+      total: 100000 + 15000000,
+      user: { connect: { id: "u1" } },
+    },
+  ];
+
+  // PRODUCT_ORDERS (pivot)
+  const productOrders: Prisma.ProductOrderCreateInput[] = [
+    {
+      id: "po1",
+      quantity: 1,
+      total: 100000,
+      order: { connect: { id: "o1" } },
+      product: { connect: { id: "p1" } },
+    },
+    {
+      id: "po2",
+      quantity: 1,
+      total: 15000000,
+      order: { connect: { id: "o1" } },
+      product: { connect: { id: "p2" } },
+    },
+  ];
+
+  // USERS
+  for (const u of users) {
+    await prisma.user.create({ data: u });
+  }
+
+  // CATEGORIES
+  for (const c of categories) {
+    await prisma.productCategory.create({ data: c });
+  }
+
+  // PRODUCTS
+  for (const p of products) {
+    await prisma.product.create({ data: p });
+  }
+
+  // ORDERS
+  for (const o of orders) {
+    await prisma.order.create({ data: o });
+  }
+
+  // PRODUCT_ORDERS
+  for (const po of productOrders) {
+    await prisma.productOrder.create({ data: po });
+  }
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => prisma.$disconnect());
